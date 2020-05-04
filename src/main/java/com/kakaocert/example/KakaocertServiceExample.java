2763b850da15f5c88a6a0c452e599e866bd74631
JohnJeong123 <frenchofkiss@gmail.com>
2020-05-04 14:25:50 +0900

    requestCMS, getCMSResult API Example 추가.

--- src/main/java/com/kakaocert/example/KakaocertServiceExample.java
+++ src/main/java/com/kakaocert/example/KakaocertServiceExample.java
@@ -9,6 +9,8 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.kakaocert.api.KakaocertException;
 import com.kakaocert.api.KakaocertService;
+import com.kakaocert.api.cms.RequestCMS;
+import com.kakaocert.api.cms.ResultCMS;
 import com.kakaocert.api.esign.RequestESign;
 import com.kakaocert.api.esign.ResultESign;
 import com.kakaocert.api.verifyauth.RequestVerifyAuth;
@@ -41,17 +43,14 @@ public class KakaocertServiceExample {
 		// 인증요청 만료시간(초), 최대값 : 99,999,999,	인증요청 만료시간(초) 내에 미인증시, 만료 상태로 처리됨
 		request.setExpires_in(60);
 		
-		// PayLoad, 이용기관이 API 요청마다 생성한 payload(메모) 값
-		request.setPayLoad("memo Info");
-		
 		// 수신자 생년월일, 형식 : YYYYMMDD
 		request.setReceiverBirthDay("19900108");
 		
 		// 수신자 휴대폰번호	
-		request.setReceiverHP("01043245117");
+		request.setReceiverHP("01012345117");
 		
 		// 수신자 성명	
-		request.setReceiverName("정요한");
+		request.setReceiverName("테스트");
 		
 		// 인증요청 메시지 부가내용, 카카오톡 인증메시지 중 상단에 표시
 		request.setTMSMessage("부가메시지 내용");
@@ -84,6 +83,8 @@ public class KakaocertServiceExample {
 		 */
 		request.setVerifyNameYN(true);
 		
+		// PayLoad, 이용기관이 API 요청마다 생성한 payload(메모) 값
+		request.setPayLoad("memo Info");
 		
 		try {
 			
@@ -134,17 +135,14 @@ public class KakaocertServiceExample {
 		// 인증요청 만료시간(초), 최대값 : 99,999,999,	인증요청 만료시간(초) 내에 미인증시, 만료 상태로 처리됨
 		request.setExpires_in(60);
 		
-		// PayLoad, 이용기관이 API 요청마다 생성한 payload(메모) 값
-		request.setPayLoad("memo Info");
-		
 		// 수신자 생년월일, 형식 : YYYYMMDD
 		request.setReceiverBirthDay("19900108");
 		
 		// 수신자 휴대폰번호	
-		request.setReceiverHP("01043245117");
+		request.setReceiverHP("01012345117");
 		
 		// 수신자 성명	
-		request.setReceiverName("정요한");
+		request.setReceiverName("테스트");
 		
 		// 인증요청 메시지 부가내용, 카카오톡 인증메시지 중 상단에 표시
 		request.setTMSMessage("부가메시지 내용");
@@ -177,6 +175,9 @@ public class KakaocertServiceExample {
 		 * false : 카카오페이가 본인인증을 통해 확보한 사용자 실명과 RecevierName 값을 비교하지 않음.	
 		 */
 		request.setVerifyNameYN(true);
+		
+		// PayLoad, 이용기관이 API 요청마다 생성한 payload(메모) 값
+		request.setPayLoad("memo Info");
 		
 		
 		try {
@@ -211,5 +212,106 @@ public class KakaocertServiceExample {
 			return "exception";
         }
         return "getVerifyAuthResult";
+    }
+	
+	/*
+	 * 카카오톡 사용자에게 자동이체 출금동의 전자서명을 요청합니다.
+	 */
+	@RequestMapping(value = "requestCMS", method = RequestMethod.GET)
+	public String requestCMS(Model m) {
+		
+		// 자동이체 출금동의 요청 정보 Object
+		RequestCMS request = new RequestCMS();
+		
+		// 고객센터 전화번호	, 카카오톡 인증메시지 중 "고객센터" 항목에 표시
+		request.setCallCenterNum("1600-9999");
+		
+		// 인증요청 만료시간(초), 최대값 : 99,999,999,	인증요청 만료시간(초) 내에 미인증시, 만료 상태로 처리됨
+		request.setExpires_in(60);
+		
+		// 수신자 생년월일, 형식 : YYYYMMDD
+		request.setReceiverBirthDay("19900108");
+		
+		// 수신자 휴대폰번호	
+		request.setReceiverHP("01012345117");
+		
+		// 수신자 성명	
+		request.setReceiverName("테스트");
+		
+		// 예금주명
+		request.setBankAccountName("예금주명");
+		
+		// 계좌번호, 이용기관은 사용자가 식별가능한 범위내에서 계좌번호의 일부를 마스킹 처리할 수 있음 예시) 371-02-6***85
+		request.setBankAccountNum("9-4324-5117-58");
+		
+		// 은행코드
+		request.setBankCode("004");
+		
+		// 납부자 식별번호, 이용기관에서 부여한 고객식별번호
+		request.setClientUserID("20200504-001");
+		
+		// 별칭코드, 이용기관이 생성한 별칭코드 (파트너 사이트에서 확인가능)
+		// 카카오톡 인증메시지 중 "요청기관" 항목에 표시
+		// 별칭코드 미 기재시 이용기관의 이용기관명이 "요청기관" 항목에 표시
+		request.setSubClientID("");
+		
+		// 인증요청 메시지 제목, 카카오톡 인증메시지 중 "요청구분" 항목에 표시
+		request.setTMSTitle("TMS Title");
+		
+		// 인증요청 메시지 부가내용, 카카오톡 인증메시지 중 상단에 표시
+		request.setTMSMessage("부가메시지 내용");
+		
+		/*
+		 * 은행계좌 실명확인 생략여부	
+		 * true : 은행계좌 실명확인 절차를 생략
+     	 * false : 은행계좌 실명확인 절차를 진행
+     	 * 
+		 * - 인증메시지를 수신한 사용자가 카카오인증 비회원일 경우,
+		 *   카카오인증 회원등록 절차를 거쳐 은행계좌 실명확인 절차이후 전자서명 가능
+		 */
+		request.setAllowSimpleRegistYN(false);
+		
+		/*
+		 * 수신자 실명확인 여부 	
+		 * true : 카카오페이가 본인인증을 통해 확보한 사용자 실명과 ReceiverName 값을 비교
+		 * false : 카카오페이가 본인인증을 통해 확보한 사용자 실명과 RecevierName 값을 비교하지 않음.	
+		 */
+		request.setVerifyNameYN(true);
+		
+		// PayLoad, 이용기관이 API 요청마다 생성한 payload(메모) 값
+		request.setPayLoad("memo Info");
+		
+		try {
+			
+			String receiptID = kakaocertService.requestCMS(ClientCode, request);
+			
+			m.addAttribute("Result", receiptID);
+			
+		} catch(KakaocertException ke) {
+			m.addAttribute("Exception", ke);
+			return "exception";
+		}
+		
+		return "result";
+		   
+	}
+	
+	/*
+     * 자동이체 출금동의 요청시 반환된 접수아이디를 통해 전자서명 결과를 확인합니다.
+     */
+	@RequestMapping(value = "getCMSResult", method = RequestMethod.GET)
+    public String getCMSResult(Model m) {
+        
+		// 출금동의 요청시 반환된 접수아이디 
+		String receiptID = "020050414231900001";
+
+        try {
+            ResultCMS result = kakaocertService.getCMSResult(ClientCode, receiptID);
+            m.addAttribute("result", result);
+        } catch(KakaocertException ke) {
+			m.addAttribute("Exception", ke);
+			return "exception";
+        }
+        return "getCMSResult";
     }
 }
